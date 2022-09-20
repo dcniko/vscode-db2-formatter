8d7f6274c0b0c56d401770737918a3b32065ad2d
Dominik Szweda <dominik.szweda@tuatara.pl>
2022-09-20 12:55:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sql Formatter
+
+Format SQL files using the [sql-formatter-plus](https://github.com/kufii/sql-formatter-plus) npm package.
+
+## Configuration
+
+**`sql-formatter.uppercase`**: Convert keywords to uppercase. Defaults to false.
+
+**`sql-formatter.linesBetweenQueries`**: Number of linebreaks between queries. Defaults to 2.
---
+++ src/extension.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const vscode = require("vscode");
+const sqlFormatter = require("sql-formatter-plus");
+
+const getSetting = (group, key, def) => {
+	const settings = vscode.workspace.getConfiguration(group, null);
+	const editor = vscode.window.activeTextEditor;
+	const language = editor && editor.document && editor.document.languageId;
+	const languageSettings = language && vscode.workspace.getConfiguration(null, null).get(`[${language}]`);
+	let value = languageSettings && languageSettings[`${group}.${key}`];
+	if (value == null) value = settings.get(key, def);
+	return value == null ? def : value;
+};
+
+const getConfig = ({ insertSpaces, tabSize }) => ({
+	indent: insertSpaces ? " ".repeat(tabSize) : "\t",
+	uppercase: getSetting("db2-formatter", "uppercase", false),
+	linesBetweenQueries: getSetting("db2-formatter", "linesBetweenQueries", 2),
+});
+
+const format = (text, config) => {
+	text = sqlFormatter.format(text, config);
+	const reg = / @ *\n*/gu;
+	return text.replace(reg, "\n@\n");
+};
+
+module.exports.activate = () =>
+	vscode.languages.registerDocumentRangeFormattingEditProvider("sql", {
+		provideDocumentRangeFormattingEdits: (document, range, options) => [
+			vscode.TextEdit.replace(range, format(document.getText(range), getConfig(options))),
+		],
+	});
